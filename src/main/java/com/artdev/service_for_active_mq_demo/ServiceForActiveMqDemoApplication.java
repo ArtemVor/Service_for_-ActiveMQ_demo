323f1c7b9044877d16b9f5137a646e06aac6e73e
artsiom.voranau <artsiom.voranau@itechart-group.com>
2022-05-23 16:41:19 +0300

    Start project

---
+++ src/main/java/com/artdev/service_for_active_mq_demo/ServiceForActiveMqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.artdev.service_for_active_mq_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceForActiveMqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceForActiveMqDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artdev/service_for_active_mq_demo/ServiceForActiveMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artdev.service_for_active_mq_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceForActiveMqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
